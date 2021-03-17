82a76ae1f736eb0b02b9f8bdbe49329b4b9a24af
danilosanches <danilo.sipoli@gmail.com>
2021-03-17 19:50:37 +0000

    It was created Estruturas and Ponteiros folders.

---
+++ IF62C/Ponteiros/aritmetica3.c
@@ -0,0 +1,25 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(){
+	
+int num[20], *pnum, diff;
+char str[30], *pstr, *pn, nome[20];
+
+//ponteiros e vetores;
+pn = nome;
+pstr = str;
+pnum = num;
+pnum += 3;  // pnum = &num[3] 
+*pnum = 10; // equivale a num[3] = 10 
+printf ("%p\n " , pstr) ;
+pstr++; 	// pstr = &str[1] 
+
+printf ( "\n \n %p \n" , pnum) ;
+printf ( "%p\n" , &num[3]) ;
+printf ( "%d\n" , num[3]) ;
+printf ( "%p\n " , pstr ) ;
+}
+
---
+++ IF62C/Ponteiros/operadorEnderecos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int i, j, k;
+	
+	printf("Endereço de i: %p\n", &i);
+  printf("Endereço de j: %p\n", &j);
+  printf("Endereço de k: %p\n", &k);
+	
+	return(0);
+}
